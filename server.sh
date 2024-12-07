a7ac31913826cb346193e985e101ca1e205a699f
Teddy Kekana <teddy.kekana@sovtech.com>
2024-12-07 10:58:37 +0200

    docs: update Kafka

--- server.sh
+++ server.sh
@@ -11,19 +11,19 @@ kill -9 $(lsof -i tcp:9092,8080,2181)
 ./bin/connect-standalone.sh config/connect-standalone.properties
 
 # Create a topic "testtopic"
-./bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 3 --topic testtopic --if-not-exists
+./bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 3 --topic testtopic --if-not-exists --command-config config/ssl-user-config.properties
 
 # Command to check for the list of topics
-./bin/kafka-topics.sh --bootstrap-server localhost:9092 --list
+./bin/kafka-topics.sh --bootstrap-server localhost:9092 --list --command-config config/ssl-user-config.properties
 
 # Command to delete topic
-./bin/kafka-topics.sh --bootstrap-server localhost:9092 --delete --topic testtopic
+./bin/kafka-topics.sh --bootstrap-server localhost:9092 --delete --topic testtopic --command-config config/ssl-user-config.properties
 
 # Command to run Kafka producer 
-./bin/kafka-console-producer.sh --topic testtopic --bootstrap-server localhost:9092
+./bin/kafka-console-producer.sh --topic testtopic --bootstrap-server localhost:9092 --command-config config/ssl-user-config.properties
 
 # Command to run Kafka consumer
-./bin/kafka-console-consumer.sh --topic testtopic --bootstrap-server localhost:9092
+./bin/kafka-console-consumer.sh --topic testtopic --bootstrap-server localhost:9092 --command-config config/ssl-user-config.properties
 
 
 ./bin/kafka-topics.sh --list --bootstrap-server localhost:9092 --command-config config/ssl-user-config.properties 
